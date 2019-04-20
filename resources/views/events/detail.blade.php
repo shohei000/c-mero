2bdf938fd743a0e2d92598f9af7001d6bc8ffdd7
saino <sainoshohei@dande-lion.co.jp>
2019-04-20 18:44:33 +0900

    YouTube切り替え

--- resources/views/events/detail.blade.php
+++ resources/views/events/detail.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
 	<div id="eventView">
+		<div class="mainMove">
+	  	<iframe id="popup-YouTube-player" width="100%" height="100%"" src="https://www.youtube.com/embed/amBB_0XMOs0?enablejsapi=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	  </div>
 	  <div class="pageKey pageKey--event">
 	  	@if($event->event_cap)
 				<img src="/storage/event/{{ $event->id }}/{{$event->event_cap}}" alt="">
@@ -20,9 +23,12 @@
 		</ul>
 		<h3 class="simp-title">出演アーティスト</h3>
 		<ul class="artistList">
+			<?php $i=0; ?>
 			@foreach($artists as $artist)
 			<li>
-				<a href="detail.html" class="artistElm">
+				{{$artist->artist_youTube}}
+				<div href="#" class="artistElm" data-youTube="{{ $artist->artist_youtube }}" data-artist-id="<?php echo $i++; ?>">
+					<span class="YouTubeTrigger">YouTubeを聞く</span>
 					<div class="artistInfo">
 						<h2 class="artistName">{{ $artist->artist_name }}</h2>
 						<ul class="artisLabels">
@@ -36,9 +42,44 @@
 							<img src="/img/logo_frame.svg">
 						@endif
 					</div>
-				</a>
+				</div>
 			</li>
 			@endforeach
 		</ul>
 	</div>
+@endsection
+
+@section('scripts')
+	<script>
+		$(function(){
+			var state = 'cap';
+			var youTubeUrlArray = [];
+			var playerWindowDOM = $('#popup-YouTube-player');
+			var playerWindow = $('#popup-YouTube-player')[0].contentWindow;
+			function stateChange(state){
+				if(state == 'YouTube'){
+					$('.pageKey').hide();
+					$('.mainMove').show();
+				}
+				if(state == 'cap'){
+					$('.pageKey').show();
+					$('.mainMove').hide();
+				}
+			}
+			$('.artistElm').each(function(index, el) {
+				var youTubeUrl = $(this).attr('data-youTube');
+				youTubeId = youTubeUrl.replace('https://www.youtube.com/watch?v=', '');
+				youTubeUrlArray.push(youTubeId);
+			});
+			$('.YouTubeTrigger').on('click',function(e){
+				var YouTubeN = $(this).parents('.artistElm').attr('data-artist-id');
+				playerWindowDOM.attr('src', 'https://www.youtube.com/embed/'+ youTubeUrlArray[YouTubeN] + '?enablejsapi=1');
+				setTimeout(() => {
+				  playerWindow.postMessage('{"event":"command","func":"'+'playVideo'+'","args":""}', '*');
+				},500)
+				stateChange('YouTube');
+				$('body,html').animate({scrollTop:0}, 500, 'swing');
+			});
+		});
+	</script>
 @endsection
