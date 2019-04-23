40d0611ca100e2f58a657da5b68b0bb506fb8b40
saino <sainoshohei@dande-lion.co.jp>
2019-04-23 18:08:52 +0900

    resetPass

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/mypage';
+    protected $redirectTo = '/user/mypage';
 
     /**
      * Create a new controller instance.
