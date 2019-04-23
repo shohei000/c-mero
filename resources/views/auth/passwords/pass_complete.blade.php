3cec7cd02ab1660ceafbeb1b0fcb6f736b9bdbb8
saino <sainoshohei@dande-lion.co.jp>
2019-04-23 18:56:01 +0900

    ac

--- resources/views/auth/passwords/pass_complete.blade.php
+++ resources/views/auth/passwords/pass_complete.blade.php
@@ -6,10 +6,11 @@
     <h2 class="form-ttl"><img src="/img/logo_text.svg" alt=""></h2>
     <main class="form-area">
       <h3 class="simp-title">メールを送信しました。</h3>
-      <p class="simp-text">
+      <p class="simp-text ac">
         入力したメールアドレスの受信ボックスをご確認ください。
       </p>
     </main>
   </div>
 </div>
 @endsection
+
