fb89710c40ded062fcc504cd72b7ffce0f993d6e
saino <sainoshohei@dande-lion.co.jp>
2019-04-23 18:23:54 +0900

    send

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -30,4 +30,9 @@ class ForgotPasswordController extends Controller
     {
         $this->middleware('guest');
     }
+
+    public function showLinkRequestForm()
+    {
+        return view('auth.passwords.email');
+    }
 }
